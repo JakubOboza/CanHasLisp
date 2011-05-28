97620ebc2995ae40da04a8abc9cfea9f97786587
Wojciech Meyer <wojciech.meyer@gmail.com>
2011-05-28 23:35:44 +0100

    Working simple parser.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,11 @@
 -- 28/05/2011 CodeJam@Cambridge
+-- TODO: make instance of Read, understand fully typeclasses.
+-- TODO: Understand TokenParser.
+
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Language (haskellDef)
+import qualified Text.ParserCombinators.Parsec.Token as P
+
 
 data SexList = SexSymbol String
     | SexString String
@@ -10,3 +17,30 @@ instance Show SexList where
     show (SexString str) = "\"" ++ str ++ "\""
     show (SexNumber num) = show num
     show (SexList lst) = "(" ++ (unwords $ map show lst) ++ ")"
+
+eatWhiteShitP = many (oneOf "\r \n\t")
+
+symbolP = do
+              symbol <- many1 letter
+              eatWhiteShitP
+              return $ SexSymbol symbol
+stringP = do
+            char '"'
+            str <- many letter                 
+            char '"'
+            eatWhiteShitP
+            return $ SexString str
+
+exprP = symbolP <|> stringP <|> listP
+
+listP = do
+            char '('
+            expr <- many exprP
+            char ')'
+            eatWhiteShitP
+            return $ SexList expr
+
+readP = do
+            sex <- listP
+            eof
+            return sex       
